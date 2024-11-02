fcc6ad51948806cb338a1bd21241b5c9ace6a80d
Victor_Alves <164104733+VictorGabriel-Dev@users.noreply.github.com>
2024-11-02 10:13:33 -0300

    Delete src/Servico.java

--- src/Servico.java
+++
@@ -1,7 +0,0 @@
-public class Servico {
-    //Declaração das array serviço
-    public int indexServico = 0 ;//contador dos serviços
-    public String[] nomeServico = new String[3];
-    public String[] descricaoServico = new String[3];
-    public double[] valorServico = new double[3];
-}
