6b5d816efa39b81792273559314a42d0153e8e2a
Victor_Alves <164104733+VictorGabriel-Dev@users.noreply.github.com>
2024-11-02 10:13:43 -0300

    Delete src/Cliente.java

--- src/Cliente.java
+++
@@ -1,8 +0,0 @@
-public class Cliente {
-    //Declaraçao de arrays cliente
-    public int indexCliente = 0;//contador dos clientes
-    public String[] nomeCliente = new String[3];
-    public String[] emailCliente = new String[3];
-    public String[] enderecoCliente = new String[3];
-    public int[] telefoneCliente = new int[3];
-}
