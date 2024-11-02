55b14679cb01c984bf7bb5e51cdd3afc01c37823
Victor_Alves <164104733+VictorGabriel-Dev@users.noreply.github.com>
2024-11-02 10:13:52 -0300

    Delete src/Agendamento.java

--- src/Agendamento.java
+++
@@ -1,6 +0,0 @@
-public class Agendamento {
-    //Declaração das Array Agendamento
-    public int indexAgendamento = 0; //pra caso eu precisar
-    public String[] agendamentoData = new String[3];
-    public String[] agendamentoHora = new String[3];
-}
